d60211b1b4eb81224262b1633354e2e756119b26
Samuele De Tomasi <el3um4s@gmail.com>
2022-10-12 15:45:57 +0200

    added cjs

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import buble from '@rollup/plugin-buble'
 export default {
   input: 'src/index.js',
   output: [{
-      file: 'dist/index.cjs.js',
+      file: 'dist/index.cjs.cjs',
       format: 'cjs',
       exports: 'default'
     },
